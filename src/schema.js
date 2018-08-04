fc0fc6077cf1384caa6366af54ab139fd1a7cd2e
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2018-08-04 12:01:46 -0500

    Creating a new records on MondoBD using Mutations

--- src/schema.js
+++ src/schema.js
@@ -2,8 +2,20 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { resolvers } from './resolvers';
 
 const typeDefs = `
+    type Product {
+        _id: ID,
+        name: String!,
+        qty: Int,
+    }
     type Query {
-        hello(msg: String!): String!
+        allProducts: [Product],
+    }
+    input ProductInput {
+        name: String!,
+        qty: Int,
+    }
+    type Mutation {
+        createProduct(input: ProductInput) : Product,
     }
 `;
 
