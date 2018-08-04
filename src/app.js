1213f9097a42de2967e6f8ad463e378e5f9430d2
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2018-08-03 19:15:36 -0500

    Creating a GraphQL Server Hello World

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
-const start = 'Welcome to node-starter-kit';
-console.log(start);
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    return res.send('Hello GraphQL Here!');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
