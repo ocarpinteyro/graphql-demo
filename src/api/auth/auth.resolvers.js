d25707c02f88f8cabd60e0b7a785277bc261763a
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2018-08-12 13:00:51 -0500

    Validate Input with Joi

--- src/api/auth/auth.resolvers.js
+++ src/api/auth/auth.resolvers.js
@@ -1,11 +1,16 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { validateUser } from '../../utils/utils';
 import { APP_SECRET } from '../../config';
 
 async function signup(_, { input }, ctx, info) {
-    const password = await bcrypt.hash(input.password, 10);
+    const { value, error } = validateUser(input);
+    if(error) {
+        throw new Error(error.message);
+    }
+    const password = await bcrypt.hash(value.password, 10);
     const user = await ctx.models.user.create({
-        email: input.email,
+        email: value.email,
         password,
     });
     const token = jwt.sign({ userId: user._id }, APP_SECRET);
