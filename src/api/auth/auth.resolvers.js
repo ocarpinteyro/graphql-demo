39e4d2141671763f5bb66be53a979f581c3d3a29
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2018-08-13 22:48:20 -0500

    Authenticate a User using a JSON Web Token

--- src/api/auth/auth.resolvers.js
+++ src/api/auth/auth.resolvers.js
@@ -23,8 +23,32 @@ async function signup(_, { input }, ctx, info) {
     };
 }
 
+async function login(parent, { input }, ctx, info) {
+    const { value, error } = validateUser(input);
+    if(error) {
+        throw new Error(error.message);
+    }
+    const user = await ctx.models.user.findOne({ email: value.email });
+    if(!user) {
+        throw new Error('No existe el usuario con el email ingresado')
+    }
+    const matched = await bcrypt.compare(value.password, user.password);
+    if(!matched) {
+        throw new Error('Password inválido')
+    }
+    const token = jwt.sign({ userId: user._id }, APP_SECRET);
+    return {
+        token,
+        user: {
+            _id: user._id,
+            email: user.email,
+        }
+    };
+}
+
 export default {
     Mutation: {
-        signup
+        signup,
+        login
     }
 };
