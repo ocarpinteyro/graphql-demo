606be1c0f053f278c65ac28c5a8105da9e118215
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2018-08-23 12:25:12 -0500

    Applying Advanced and Modern Cursor Based Pagination.

--- src/api/product/product.model.js
+++ src/api/product/product.model.js
@@ -14,7 +14,7 @@ const ProductSchema = new Schema({
         ref: "User",
         type: Schema.Types.ObjectId,
         required: true,
-    }
-});
+    },
+}, { timestamps: true });
 
 export default mongoose.model("product", ProductSchema);
